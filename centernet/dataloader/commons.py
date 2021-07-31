ae6312dcb211e468192525259dedfdb76cee106c
soumik.rakshit <soumik.rakshit@ignitarium.com>
2021-08-01 00:25:41 +0530

    Added preprocessor

---
+++ centernet/dataloader/commons.py
@@ -0,0 +1,22 @@
+import numpy as np
+from math import sin, cos
+
+
+def rotate(x, angle):
+    x = x + angle
+    x = x - (x + np.pi) // (2 * np.pi) * 2 * np.pi
+    return x
+
+
+def preprocess_regression_targets(coordinate, flip: bool = False):
+    if flip:
+        for k in ['x', 'pitch', 'roll']:
+            coordinate[k] = -coordinate[k]
+    for name in ['x', 'y', 'z']:
+        coordinate[name] = coordinate[name] / 100
+    coordinate['roll'] = rotate(coordinate['roll'], np.pi)
+    coordinate['pitch_sin'] = sin(coordinate['pitch'])
+    coordinate['pitch_cos'] = cos(coordinate['pitch'])
+    coordinate.pop('pitch')
+    coordinate.pop('id')
+    return coordinate
