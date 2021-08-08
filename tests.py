790d4509412af4f424e8843da193285030e621a5
soumik.rakshit <soumik.rakshit@ignitarium.com>
2021-08-08 18:15:33 +0530

    Updated testcases

--- tests.py
+++ tests.py
@@ -1,7 +1,12 @@
 import os
+import numpy as np
 import pandas as pd
+from PIL import Image
 from glob import glob
 from unittest import TestCase
+
+from centernet.commons import read_camera_intrinsic
+from centernet.dataloader import Preprocessor, PostProcess
 
 
 class TestDataset(TestCase):
@@ -11,8 +16,13 @@ class TestDataset(TestCase):
             len(glob('./data/pku-autonomous-driving/*')) > 0)
 
     def test_camera_intrinsic(self):
-        self.assertTrue(
-            os.path.isfile('./data/pku-autonomous-driving/camera/camera_intrinsic.txt'))
+        camera_intrinsic_file = './data/pku-autonomous-driving/camera/camera_intrinsic.txt'
+        self.assertTrue(os.path.isfile(camera_intrinsic_file))
+        (fx, fy, cx, cy), _, _ = read_camera_intrinsic('./data/pku-autonomous-driving/')
+        self.assertTrue(fx == 2304.5479)
+        self.assertTrue(fy == 2305.8757)
+        self.assertTrue(cx == 1686.2379)
+        self.assertTrue(cy == 1354.9849)
     
     def test_car_model_files(self):
         self.assertTrue(
@@ -31,3 +41,30 @@ class TestDataset(TestCase):
         self.assertTrue(len(train_dataframe) > 0)
         self.assertTrue('ImageId' in train_dataframe)
         self.assertTrue('PredictionString' in train_dataframe)
+
+
+class TestPreprocessor(TestCase):
+
+    def test_shapes(self):
+        train_dataframe = pd.read_csv('./data/pku-autonomous-driving/train.csv')
+        preprocessor = Preprocessor(
+            dataset_path='./data/pku-autonomous-driving/',
+            image_height=320, image_width=1024, model_scale=8
+        )
+        sample_dataset = train_dataframe.sample(n=1)
+        sample_image_id = list(sample_dataset['ImageId'])[0]
+        sample_prediction_string = list(sample_dataset['PredictionString'])[0]
+        sample_image = np.array(Image.open(
+            './data/pku-autonomous-driving/train_images/{}.jpg'.format(sample_image_id)))
+        preprocessed_image = preprocessor.preprocess_image(sample_image, flip=False)
+        self.assertTrue(preprocessed_image.shape == (320, 1024, 3))
+        preprocessed_image = preprocessor.preprocess_image(sample_image, flip=True)
+        self.assertTrue(preprocessed_image.shape == (320, 1024, 3))
+        mask, regression_target = preprocessor.get_targets(
+            sample_image, sample_prediction_string, flip=False)
+        self.assertTrue(mask.shape == (40, 128))
+        self.assertTrue(regression_target.shape == (40, 128, 7))
+        mask, regression_target = preprocessor.get_targets(
+            sample_image, sample_prediction_string, flip=True)
+        self.assertTrue(mask.shape == (40, 128))
+        self.assertTrue(regression_target.shape == (40, 128, 7))
